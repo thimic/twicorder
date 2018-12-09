66c83c24e3efb1e38b2720bcb1ae0a905e3373aa
Michael Thingnes <thimic@gmail.com>
2018-12-09 21:00:54 +1300

    Added backfill function to backfill MongoDB from already crawled tweets

--- python/twicorder/mongo.py
+++ python/twicorder/mongo.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import glob
+import json
+import os
+
+from datetime import datetime
+
 from pymongo import MongoClient, TEXT
+
+from twicorder.config import Config
+from twicorder import utils
 
 
 def create_collection(db_name='slpng_giants', collection_name='tweets'):
@@ -39,3 +48,46 @@ def create_collection(db_name='slpng_giants', collection_name='tweets'):
     collection.create_index('user.statuses_count')
     collection.create_index([('full_text', TEXT)])
     return collection
+
+
+def backfill(db_name='slpng_giants', collection_name='tweets'):
+    logger = utils.FileLogger.get()
+    tweets = create_collection(db_name, collection_name)
+
+    config = Config.get()
+    save_dir = os.path.expanduser(config['save_dir'])
+
+    paths = glob.glob(os.path.join(save_dir, '**', '*.t*'), recursive=True)
+    t0 = datetime.now()
+    for idx, path in enumerate(paths):
+        try:
+            for lidx, line in enumerate(utils.readlines(path)):
+                try:
+                    data = json.loads(line)
+                except Exception:
+                    logger.exception(
+                        f'Backfill: Unable to read line {path}:{lidx + 1}'
+                    )
+                    continue
+                else:
+                    if data.get('delete'):
+                        continue
+                    if os.path.basename(os.path.dirname(path)) == 'stream':
+                        data = utils.timestamp_to_datetime(data)
+                    data = utils.stream_to_search(data)
+                    tweets.replace_one({'id': data['id']}, data, upsert=True)
+            t_delta = datetime.now() - t0
+            average = t_delta / (idx + 1)
+            remaining = str((len(paths) - (idx + 1)) * average).split('.')[0]
+
+            logger.info(
+                f'{idx + 1}/{len(paths)} '
+                f'{remaining} '
+                f'{os.sep.join(path.split(os.sep)[-3:])}'
+            )
+        except Exception:
+            logger.exception(f'Backfill: Unable to read file: {path}')
+
+
+if __name__ == '__main__':
+    backfill()
