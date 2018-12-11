4a87b03fe0cfcec2876c005ba2585b1dfc16c870
Michael Thingnes <thimic@gmail.com>
2018-12-11 21:41:09 +1300

    Add fallbacks if unable to connect to Mongo

--- python/twicorder/mongo.py
+++ python/twicorder/mongo.py
@@ -31,29 +31,29 @@ def create_collection(db_name='slpng_giants', collection_name='tweets'):
     try:
         client = MongoClient()
         db = client[db_name]
+        if collection_name in db.list_collection_names():
+            return db[collection_name]
+        collection = db[collection_name]
+        collection.create_index('id', unique=True)
+        collection.create_index('created_at')
+        collection.create_index('retweet_count')
+        collection.create_index('favorite_count')
+        collection.create_index('in_reply_to_status_id')
+        collection.create_index('in_reply_to_user_id')
+        collection.create_index('in_reply_to_screen_name')
+        collection.create_index('entities.hashtags')
+        collection.create_index('user.created_at')
+        collection.create_index('user.screen_name')
+        collection.create_index('user.id')
+        collection.create_index('user.followers_count')
+        collection.create_index('user.favourites_count')
+        collection.create_index('user.verified')
+        collection.create_index('user.statuses_count')
+        collection.create_index([('full_text', TEXT)])
+        return collection
     except Exception:
         logger.exception('Unable to connect to MongoDB: ')
         return
-    if collection_name in db.list_collection_names():
-        return db[collection_name]
-    collection = db[collection_name]
-    collection.create_index('id', unique=True)
-    collection.create_index('created_at')
-    collection.create_index('retweet_count')
-    collection.create_index('favorite_count')
-    collection.create_index('in_reply_to_status_id')
-    collection.create_index('in_reply_to_user_id')
-    collection.create_index('in_reply_to_screen_name')
-    collection.create_index('entities.hashtags')
-    collection.create_index('user.created_at')
-    collection.create_index('user.screen_name')
-    collection.create_index('user.id')
-    collection.create_index('user.followers_count')
-    collection.create_index('user.favourites_count')
-    collection.create_index('user.verified')
-    collection.create_index('user.statuses_count')
-    collection.create_index([('full_text', TEXT)])
-    return collection
 
 
 def backfill(path=None, db_name='slpng_giants', collection_name='tweets'):
